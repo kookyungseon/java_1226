e5ba9692e4f3801c541a8b6e97cda0846314718c
kookyungseon <114800024+kookyungseon@users.noreply.github.com>
2024-01-15 15:14:00 +0900

    day14

---
+++ workspace/day14/src/ambigulty/ClassA.java
@@ -0,0 +1,9 @@
+package ambigulty;
+
+public class ClassA {
+	
+	public void printText() {
+		System.out.println("ClassA printText()");
+	}
+
+}
---
+++ workspace/day14/src/ambigulty/Inter3.java
@@ -0,0 +1,9 @@
+package ambigulty;
+
+public interface Inter3 {
+	
+	default void printText() {
+		System.out.println("Inter3 printText()");
+	}
+
+}
